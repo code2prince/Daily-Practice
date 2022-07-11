af0d2576560e4f37bdabd27c243b62846af62252
github_username <princekumarsharma365@gmail.com>
2022-07-11 16:32:06 +0530

    swapping without 3rd variable

---
+++ dailysolvingAlgo/swapping.js
@@ -0,0 +1,13 @@
+// swappping without 3rd variable
+
+let a=10;
+let b=20;
+// a=a+b;
+// b=a-b;
+// a=a-b;
+
+a=a*b;
+b=a/b;
+a=a/b;
+console.log("a:", a ,"b:", b);
+console.log(`a: ${a} b: ${b}`);
