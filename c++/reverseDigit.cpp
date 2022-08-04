ae7b6464969afc7a866691379bf702ed440ea64e
github_username <princekumarsharma365@gmail.com>
2022-08-04 23:14:08 +0530

    reverse a no using for loop

--- c++/reverseDigit.cpp
+++ c++/reverseDigit.cpp
@@ -1,15 +1,29 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     int rev=0;
+//     cout<<"Enter no:";
+//     cin>>n;
+//     while(n>0){
+
+//         rev=(rev*10) + n%10;
+//         n=n/10;
+//     }
+//     cout<<"Reverse:"<<rev;
+//     return 0;
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    int rev=0;
-    cout<<"Enter no:";
-    cin>>n;
-    while(n>0){
-
-        rev=(rev*10) + n%10;
-        n=n/10;
+    int m;
+    int reverse=0;
+    cout<<"Enter the no:";
+    cin>>m;
+    for(; m>0; m=m/10){
+        reverse=(reverse*10) +m%10;
     }
-    cout<<"Reverse:"<<rev;
+    cout<<"reverse:"<<reverse;
     return 0;
 }
