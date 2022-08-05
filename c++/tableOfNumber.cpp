a775af50357faa30e95161f4af346d4525641415
github_username <princekumarsharma365@gmail.com>
2022-08-05 21:26:11 +0530

    table of a given no

---
+++ c++/tableOfNumber.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    cout<<"Enter the no:";
+    cin>>i;
+    for(j=1; j<=10; j++){
+        cout<<i*j<<endl;
+    }
+    return 0;
+}
+
+
+//while loop
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i,j;
+//     cout<<"Enter a no:";
+//     cin>>i;
+//     j=1;
+//     while(j<=10){
+//         cout<<i*j<<endl;
+//         j++;
+//     }
+//     return 0;
+// }
