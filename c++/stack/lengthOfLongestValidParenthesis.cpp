526d7b6681bbbce0af5b4a8d03491ef8f03b0c58
github_username <princekumarsharma365@gmail.com>
2022-08-17 12:39:51 +0530

    length of longest valid  parenthesis

---
+++ c++/stack/lengthOfLongestValidParenthesis.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int findMaxLength(string str){
+    int n= str.length();
+    stack<int>s;
+    s.push(-1);
+    int result=0;
+    for(int i=0; i<n; i++){
+        if(str[i] =='(')
+            s.push(i);
+        else
+            {
+                if(!s.empty())
+                {
+                    s.pop();
+                    result=max(result, i-s.top());
+                }
+                else
+                    s.push(i);
+            }
+    }
+    return result;
+}
+int main(){
+    string str="(()())(";
+    cout<<"length of longest valid parenthesis:"<<findMaxLength(str)<<endl;
+    return 0;
+}
+
+ // length of longest valid parenthesis: 4               
+            
