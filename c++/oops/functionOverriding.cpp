2577f3a82e7ba20d3cdf47bf1a708edea18bf124
github_username <princekumarsharma365@gmail.com>
2022-08-23 12:24:40 +0530

    method overridding

---
+++ c++/oops/functionOverriding.cpp
@@ -0,0 +1,27 @@
+// function / method overridding  is a feature that allow you to redefine the parent class method in the child class based on its requirement.
+
+#include<iostream>
+using namespace std;
+
+//parent class
+class Animal{
+    public:
+    void speak(){
+        cout<<"Speaking";
+    }
+};
+// child class
+class Dog{
+    public:
+    void speak(){
+        cout<<"Barking";
+    }
+};
+
+int main(){
+    Dog obj;
+    obj.speak();
+    return 0;
+}
+
+// Barking
