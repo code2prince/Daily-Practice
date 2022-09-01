943ba6867ab42d6cfdf27fb6a5bfe196f1fcafbd
github_username <princekumarsharma365@gmail.com>
2022-09-02 00:17:41 +0530

    frequery of char

---
+++ c++/frequencyCharcter.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str= "I am Prince Kumar Sharma";
+    
+    // string str;
+    // cout<<"enter string:";
+    // cin>>str;
+
+    char check = 'm';
+    int count=0;
+    for(int i=0; i<str.length(); i++){
+        if(str[i]== check){
+            count++;
+        }
+    }
+    cout<<"Frequency of character "<< check<<" into string :" <<count;
+    return 0;
+}
+
+// Frequency of character m into string :3
