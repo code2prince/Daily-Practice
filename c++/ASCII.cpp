c46d70688f6c3f4c06f59b31e55858fa84572fff
github_username <princekumarsharma365@gmail.com>
2022-09-01 23:35:04 +0530

    ASCII value of char

---
+++ c++/ASCII.cpp
@@ -0,0 +1,16 @@
+// find ASCII VALUE of a char 
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    char c;
+    cout<<"Enter a Character:";
+    cin>>c;
+
+    cout<<"ASCII value is:" <<int(c);
+    return 0;
+}
+
+/// Enter a Character:B
+//  66
