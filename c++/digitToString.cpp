ef1acaf489231757bc9098b5df978155f152aa81
github_username <princekumarsharma365@gmail.com>
2022-09-04 11:42:01 +0530

    digit to string in cpp

---
+++ c++/digitToString.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+void sayDigit(int n, string arr[]){
+
+// base case
+if(n==0)
+return;
+
+// processing 
+int digit=n%10;
+n=n/10;
+
+// recursive call
+
+sayDigit(n, arr);
+cout<<arr[digit]<<endl;
+
+}
+
+
+
+int main(){
+string arr[10]={"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+int n;
+cout<<"Enter no:";
+cin>>n;
+
+sayDigit(n, arr);
+return 0;
+
+}
+
+// Enter no:360
+// three
+// six
+// zero
