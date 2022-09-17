7ee5b52b2493887a78b9b1e47c36026b6eac1906
github_username <princekumarsharma365@gmail.com>
2022-09-17 22:05:13 +0530

    triangular pattern in cpp

---
+++ c++/pattern/triangularPattern.cpp
@@ -0,0 +1,25 @@
+// triangular pattern
+
+#include<iostream>
+using namespace std;
+int main(){
+    int row;
+    cout<<"Enter no of rows:";
+    cin>>row;
+
+    for(int i=1; i<=row; ++i){
+        for(int j=1; j<=i; ++j){
+            cout<<"*";
+        }
+    cout<<"\n";
+    }
+    return 0;
+}
+
+
+// Enter no of rows:5
+// *
+// **
+// ***
+// ****
+// *****
