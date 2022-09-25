2f2b366f841765df8c065d4218eb284e8f0788a4
github_username <princekumarsharma365@gmail.com>
2022-09-25 18:42:59 +0530

    celsius to fahrenheit

---
+++ temperature/celsiusToFahrenheit.cpp
@@ -0,0 +1,17 @@
+// celsius to fahrenheit
+
+#include<iostream>
+using namespace std;
+int main(){
+    float celsius, fahrenheit;
+
+    cout<<"Enter celsius:";
+    cin>>celsius;
+
+    fahrenheit= (celsius *1.8) +32;
+    cout<<"celsius to farenheit is:"<<fahrenheit;
+    return 0;
+}
+
+// Enter celsius:37
+// celsius to farenheit is:98.6
