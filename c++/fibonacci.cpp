ab4cfbb42383302872c219162dece27ad428b7f9
github_username <princekumarsharma365@gmail.com>
2022-10-04 14:13:16 +0530

    fibonacci no

--- c++/fibonacci.cpp
+++ c++/fibonacci.cpp
@@ -1,19 +1,50 @@
+//fibonacci series
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter a value of n:";
+//     cin>>n;
+
+//     int a=0;
+//     int b=1;
+//     cout<<a <<" ";
+//     cout<<b<<" ";
+//     for (int i=1; i<n; i++){            ..........     ///or for(int i=2; i<=n; i++)  --->>because 2 value already assigning...0 and 1
+//         int fibo=a+b;
+//         cout<<fibo<<" ";
+//         a=b;
+//         b=fibo;
+
+//     }
+// }
+
+
+// Enter a value of n:10
+// 0 1 1 2 3 5 8 13 21 34 55 
+
+
+// fibonacci no
+
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"Enter a value of n:";
+    int n,fib;
+    cout<<"enter no:";
     cin>>n;
 
-    int a=0;
-    int b=1;
-    cout<<a <<endl;
-    cout<<b<<endl;
-    for (int i=0; i<=n; i++){
-        int fibo=a+b;
-        cout<<fibo<<endl;
+    int a=0,b=1;
+    for(int i=2; i<=n; i++){
+         fib=a+b;
+       
         a=b;
-        b=fibo;
-
+        b=fib;
     }
+     cout<<fib;
+    return 0;
 }
+
+
+// enter no:10
+// 55
