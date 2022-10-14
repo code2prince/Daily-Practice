540cbf79fec8eb60c0d23a4d898c0551588f847b
github_username <princekumarsharma365@gmail.com>
2022-10-14 15:26:03 +0530

    print A to Z value with and without ASCII

---
+++ c++/AtoZ.cpp
@@ -0,0 +1,60 @@
+// print A to Z values
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(char i='A'; i<='Z'; i++){
+        cout<< i << " ";
+    }
+    return 0;
+}
+
+// A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(char i='a'; i<='z'; i++){
+//         cout<< i << " ";
+//     }
+//     return 0;
+// }
+
+// a b c d e f g h i j k l m n o p q r s t u v w x y z 
+
+
+
+
+// ASCII code  65 to 90  >>> A to Z
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=65; i<=90 ; i++){
+//         cout<< char(i) << " ";
+//     }
+//     return 0;
+// }
+
+/// A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 
+
+
+
+  
+ 
+
+
+// ASCII code    a to z  >>>   97 to 122
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=97; i<=122; i++){
+//         cout<< char(i) << " ";
+//     }
+//     return 0;
+// }
+
+// a b c d e f g h i j k l m n o p q r s t u v w x y z 
