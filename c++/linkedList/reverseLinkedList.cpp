1f03dd83619a3a3e7d4097303ccb1a8fb09f01ef
github_username <princekumarsharma365@gmail.com>
2022-11-02 23:48:12 +0530

    reverse linked list

--- c++/linkedList/reverseLinkedList.cpp
+++ c++/linkedList/reverseLinkedList.cpp
@@ -14,3 +14,29 @@
 //             return newHead;
 //         }
 // }
+
+
+
+
+
+
+// class Solution
+// {
+//     public:
+//     //Function to reverse a linked list.
+//     struct Node* reverseList(struct Node *head)
+//     {
+//         Node* new_head=NULL;
+//         while(head!= NULL){
+//             Node* temp=head;
+//             head=head->next;
+//             temp->next= new_head;
+//             new_head=temp;
+            
+//         }
+//         return new_head;
+        
+//     }
+    
+// };
+   
