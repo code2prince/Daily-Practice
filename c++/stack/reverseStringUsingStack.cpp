98678fb7eeeb729ffa5e946a9431d63b045ed378
github_username <princekumarsharma365@gmail.com>
2022-11-03 21:12:49 +0530

    reverse stack using stack

--- c++/stack/reverseStringUsingStack.cpp
+++ c++/stack/reverseStringUsingStack.cpp
@@ -21,3 +21,39 @@ int main(){
     cout<<ans;
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//     string s="prince";
+//     stack<char> st;
+//     for(int i=0; i<s.length(); i++){
+//         char ch=s[i];
+//         st.push(ch);
+//     }
+    
+//     while(!st.empty()){
+//         cout<<st.top();
+//         st.pop();
+//     }
+//     return 0;
+// }
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//     string s="prince";
+//     stack<char> st;
+//     for(char x:s){
+//         st.push(x);
+//     }
+//     while(!st.empty()){
+//         cout<<st.top();
+//         st.pop();
+//     }
+
+//     return 0;
+// }
