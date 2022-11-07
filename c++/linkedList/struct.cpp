209f8aa58cc1c54108bedf778364f4d4f243e6cb
github_username <princekumarsharma365@gmail.com>
2022-11-08 00:32:56 +0530

    stuct basic

--- c++/linkedList/struct.cpp
+++ c++/linkedList/struct.cpp
@@ -1,24 +1,63 @@
 // basic struct
 
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+// struct student{
+//     int age;
+//     int roll;
+//     string name;
+//     int mobile;
+
+
+// };
+// int main(){
+//     student obj;
+//     obj.name="Prince";
+//     obj.age=21;
+//     obj.mobile=873091666;
+//     obj.roll=18;
+
+//     cout<<" name: " <<obj.name << " age: "<< obj.age<< " mobile: "<< obj.mobile<< " roll: "<<obj.roll<<endl;
+//     return 0;
+// }
+
+//  name: Prince age: 21 mobile: 873091666 roll: 18
+
+
+
+#include<iostream>
 using namespace std;
 struct student{
     int age;
     int roll;
     string name;
-    int mobile;
 
-
+    void detail(){
+        cout<<"name: " <<name<<endl;
+        cout<<"age: "<<age<<endl;
+        cout<<"roll: "<<roll<<endl;
+    }
 };
 int main(){
     student obj;
-    obj.name="Prince";
+    obj.name="Prince sharma";
     obj.age=21;
-    obj.mobile=873091666;
     obj.roll=18;
 
-    cout<<" name: " <<obj.name << " age: "<< obj.age<< " mobile: "<< obj.mobile<< " roll: "<<obj.roll<<endl;
+    obj.detail();
+
+    student prem={22,19,"Prem"};
+    prem.detail();
+
+
     return 0;
 }
 
-//  name: Prince age: 21 mobile: 873091666 roll: 18
+// name: Prince sharma
+// age: 21
+// roll: 18
+
+
+// name: Prem
+// age: 22
+// roll: 19
