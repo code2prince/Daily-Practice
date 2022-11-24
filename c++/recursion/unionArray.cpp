235e0ced3d19f9518e7a72713e2caabebb0e73cd
github_username <princekumarsharma365@gmail.com>
2022-11-24 11:35:20 +0530

    union of two sorted araay geeks

--- c++/recursion/unionArray.cpp
+++ c++/recursion/unionArray.cpp
@@ -35,3 +35,27 @@ int main() {
 }
 
 // 1 2 3 4 5 6 7
+
+
+
+
+// class Solution{
+//     public:
+//     //arr1,arr2 : the arrays
+//     // n, m: size of arrays
+//     //Function to return a list containing the union of the two arrays. 
+//     vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+//     {
+//         //Your code here
+//         //return vector with correct order of elements
+//      set<int>s;
+//      for(int i=0; i<n; i++){
+//          s.insert(arr1[i]);
+//      }
+//      for(int i=0; i<m; i++){
+//          s.insert(arr2[i]);
+//      }
+//      vector<int>v{s.begin(), s.end()};
+//      return v;
+//     }
+// };
