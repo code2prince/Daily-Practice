af0e8b8f7584faf1d251cb6a70d09599fe95f3c1
github_username <princekumarsharma365@gmail.com>
2022-12-13 18:39:45 +0530

    find unique array using 2 method 1st using XOR and 2nd using hashmap

--- c++/FindUniqueArrray.cpp
+++ c++/FindUniqueArrray.cpp
@@ -11,3 +11,31 @@ int main(){
    cout<<"ans:"<<ans;
 
 }
+
+
+
+
+// 2nd approach  >> hashmap
+
+/*
+ unordered_map<int,int>m;
+      for(int i=0; i<n; i++){
+          m[arr[i]]++;
+      }
+      for(int i=0; i<n; i++){
+          int key=arr[i];
+
+          >>>>>>>>>>>>>>>>>>
+          // auto temp= m.find(key);
+          // if(temp->second==1){
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+            >>or 
+
+            if(m[arr[i]]==1){      // or >> if(m[key]==1){
+              return key;
+          }
+      }
+      return 0;
+    } 
+*/
