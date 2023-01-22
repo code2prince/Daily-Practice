0507dce58f0e3163b3d44cc6cfc0f55b0691a1ad
github_username <princekumarsharma365@gmail.com>
2023-01-22 18:12:51 +0530

    check given string is isogram or not

---
+++ c++/String/checkStringIsogrm.cpp
@@ -0,0 +1,59 @@
+// Check given string is isogram or not
+
+/*
+Given a string S of lowercase alphabets, check if it is isogram or not. An Isogram is a string in which no letter occurs more than once.
+
+Example 1:
+
+Input:
+S = machine
+Output: 1
+Explanation: machine is an isogram
+as no letter has appeared twice. Hence
+we print 1.
+
+
+
+Example 2:
+
+Input:
+S = geeks
+Output: 0
+Explanation: geeks is not an isogram
+as 'e' appears twice. Hence we print 0.
+
+*/
+
+
+
+/*
+class Solution
+{
+    public:
+    //Function to check if a string is Isogram or not.
+    bool isIsogram(string s)
+    {
+        //Your code here
+        // unordered_map<char,int>m;
+        // for(int i; i<s.length(); i++){
+        //     m[s[i]]++;
+        // }
+        // for(int i=0; i<m.size(); i++){
+        //     if(m[s[i]]>1)
+        //     return false;
+        // }
+        // return true;
+        
+
+        // 2nd approach ************
+        sort(s.begin(), s.end());
+        for(int i=0; i<s.length(); i++){
+            if(s[i]==s[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+*/
