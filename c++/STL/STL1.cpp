45b526383e9558882d1f2b340f4fbf6c99810d78
github_username <princekumarsharma365@gmail.com>
2023-01-30 17:56:13 +0530

    stl 1 vector

---
+++ c++/STL/STL1.cpp
@@ -0,0 +1,97 @@
+/*
+Input:
+The first line of input contains an integer T denoting the no of test cases . Then T test cases follow. The first line of input contains an integer Q denoting the no of queries . Then in the next line are Q space separated queries .
+A query can be of five types 
+1. a x (Adds an element x to the vector A at the end )
+2. b (Sorts the vector A in ascending order )
+3. c (Reverses the vector A)
+4. d (prints the size of the vector)
+5. e (prints space separated values of the vector)
+5. f  (Sorts the vector A in descending order)
+
+*/
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// void add_to_vector(vector<int> &A,int x) {
+
+
+
+//  A.push_back(x);
+
+// }
+
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+// /*sort the vector A in ascending order*/
+
+// void sort_vector_asc(vector<int> &A){
+
+//  //Your code here
+
+//  sort(A.begin(),A.end());
+
+// }
+
+//********************************************************
+
+// /*reverses the vector A*/
+
+// void reverse_vector(vector<int> &A) {
+
+//  //Your code here
+
+//  reverse(A.begin(),A.end());
+
+// }
+
+
+// >>>>>>>>>>********************************************
+// /*returns the size of the vector  A */
+
+// int size_of_vector(vector<int> &A) {
+
+//  //Your code here
+
+//  int n=A.size();
+
+//  return n;
+
+// }
+
+
+//******************************************************************
+ 
+
+// /*sorts the vector A in descending order*/
+
+// void sort_vector_desc(vector<int> &A) {
+
+//  //Your code here
+
+//  sort(A.rbegin(),A.rend());
+
+// }
+
+ 
+
+
+// *********************************************************888
+
+// /*prints space separated elements of vector A*/
+
+// void print_vector(vector<int> &A) {
+
+//  //Your code here
+
+//  for(auto x:A) {   /// or   ***********>>>>>>>> for(int i=0 ; i<A.size(); i++)  cout<<A[i]<<" ";
+
+//      cout<<x<<" ";
+
+//  }
+
+// }
