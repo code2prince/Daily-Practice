0adf94d323a78ab0ff29b8da16eedd01d3d57854
github_username <princekumarsharma365@gmail.com>
2023-02-11 19:38:29 +0530

    balanace parenthesis

--- c++/stack/balancedParenthesis.cpp
+++ c++/stack/balancedParenthesis.cpp
@@ -48,6 +48,28 @@ bool isValid(string str){
 }
 
 
+// or
+ /*
+  bool isValid(string x)
+  {
+     stack<char>st;
+        for(auto c:x){
+            if(st.empty())
+            st.push(c);
+           else if((st.top()=='(' && c==')') || (st.top()=='{' && c=='}') || (st.top()=='[' && c==']')){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        if(st.size()==0)
+            return true;
+        else
+        return false;
+    }
+ */
+
 
 int main() {
     string str="{[()]}";
@@ -59,3 +81,48 @@ int main() {
     }
     return 0;
 }
+
+
+
+
+//******** without stack   **************************
+
+//>>>>>>>>>not suitable for all test cases
+
+/*
+#include <bits/stdc++.h>
+//#include <string>
+using namespace std;
+
+bool isValidParenthesis( string str) {
+  int b1 = 0, b2=0, b3=0;
+  for (char c : str) {
+
+    if(c=='(')b1++;
+    if(c=='[')b2++;
+    
+    if(c==')')b1--;
+    if(c==']')b2--;
+    
+    if(c=='{')b3++;
+    if(c=='}')b3--;
+    
+    if (b1 < 0 || b2<0 || b3<0) return false;
+  }
+  return b1 == 0;
+  return b2==0;
+  return b3==0;
+  
+}
+
+int main() {
+  string str = "[({})[]()]";
+  if (isValidParenthesis(str)) {
+    cout << "Valid Parenthesis" << endl;
+  } else {
+    cout << "Invalid Parenthesis" << endl;
+  }
+  return 0;
+}
+
+*/
