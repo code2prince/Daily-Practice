b92062ba981fe6558953c2fb47e06c04328a8bec
github_username <princekumarsharma365@gmail.com>
2023-02-13 17:25:16 +0530

    search into a matrix

--- c++/Matrix/searchInMatrix.cpp
+++ c++/Matrix/searchInMatrix.cpp
@@ -65,3 +65,25 @@ public:
 	}
 };
 */
+
+#include<iostream>
+using namespace std;
+int main(){
+	int matrix[3][3]={{1,2,3},
+				{4,5,6},
+				{7,8,9}};
+	
+
+	int k=7;
+	for(int i=0; i<3; i++){
+		for(int j=0; j<3; j++){
+			if(matrix[i][j]==k){
+				cout<<"present";
+				break;
+			}
+			cout<<"not";
+			break;
+		}
+	}
+
+}
