5791b439e7a84b95cda9d02e0692cf9108b76509
github_username <princekumarsharma365@gmail.com>
2023-02-14 18:12:18 +0530

    linearly n to 1 using recursion

--- c++/recursion/recursionDay1.cpp
+++ c++/recursion/recursionDay1.cpp
@@ -18,22 +18,24 @@
 //     return 0;
 // }
 
-#include<iostream>
-using namespace std;
-void print(int i, int n){
-    if(i>n){
-        return;
-    }
-    cout<<"Prince"<<endl;
-    print(i+1,n);
-}
-int main(){
-    int n;
-    cout<<"Enter n: ";
-    cin>>n;
-    int i=1;
-    print(i,n);
-}
+
+//**********
+// #include<iostream>
+// using namespace std;
+// void print(int i, int n){
+//     if(i>n){
+//         return;
+//     }
+//     cout<<"Prince"<<endl;
+//     print(i+1,n);
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter n: ";
+//     cin>>n;
+//     int i=1;
+//     print(i,n);
+// }
 /*
 Enter n: 5
 Prince
@@ -57,7 +59,7 @@ void print(int i, int n){
 // Base case
     if(i>n)
      return;
-     
+
     cout<<i<<endl;
     print(i+1,n);
 };
@@ -102,3 +104,34 @@ ENter n: 5
 
 
 */
+
+
+
+// reverse order n to 1
+
+#include<iostream>
+using namespace std;
+void print( int i,int n){
+// Base case
+    if(i<1){
+        return;
+    }
+    cout<<i<<endl;
+    print(i-1,n);
+   
+    
+};
+int main(){
+    int n;
+    cout<<"ENter n: ";
+    cin>>n;
+    print(n,n);
+    return 0;
+}
+
+// ENter n: 5 
+// 5
+// 4
+// 3
+// 2
+// 1
