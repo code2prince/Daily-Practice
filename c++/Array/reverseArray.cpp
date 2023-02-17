8eb07199148268d017f45280f38c2d87439f2057
github_username <princekumarsharma365@gmail.com>
2023-02-17 10:12:06 +0530

    reverse array

--- c++/Array/reverseArray.cpp
+++ c++/Array/reverseArray.cpp
@@ -1,19 +1,58 @@
 // reverse an array
 
-#include <iostream>
-using namespace std;
-int main() {
-    int arr[]={1,2,3,4,6};
-    int n=sizeof(arr)/sizeof(arr[0]);
-   for(int i=0; i<n/2; i++){
-       int temp=arr[i];
-       arr[i]=arr[n-i-1];
-       arr[n-i-1]=temp;
-   }
-   for(int i=0; i<n ;i++){
-       cout <<arr[i]<< " ";
-   }
-    return 0;
-}
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int arr[]={1,2,3,4,6};
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//    for(int i=0; i<n/2; i++){
+//        int temp=arr[i];
+//        arr[i]=arr[n-i-1];
+//        arr[n-i-1]=temp;
+//    }
+//    for(int i=0; i<n ;i++){
+//        cout <<arr[i]<< " ";
+//    }
+//     return 0;
+// }
 
 //6 4 3 2 1 
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+void rev(int arr[],int s, int e){
+    while(s<e){
+        //swap(arr[s],arr[e]);
+        int temp=arr[s];
+        arr[s]=arr[e];
+        arr[e]=temp;
+        s++;
+        e--;
+    }
+}
+int main(){
+    int arr[]={1,2,3,4,5,6};
+        int n=6;
+        rev(arr,0,n-1);
+        for(int i=0; i<n; i++){
+            cout<<arr[i]<<" ";
+        }
+}
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[]={1,2,3,4,5};
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     for(int i=n-1; i>=0; i--){
+//         cout<<arr[i]<<" ";
+//     }
+// }
