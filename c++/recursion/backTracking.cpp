7a84dac2e0405c60da0de393fc705265c960d583
github_username <princekumarsharma365@gmail.com>
2023-02-21 16:10:01 +0530

    1 to N using backtracking

--- c++/recursion/backTracking.cpp
+++ c++/recursion/backTracking.cpp
@@ -1,18 +1,34 @@
 // Backtracking 1 to N;
 
-#include <iostream>
-using namespace std;
-void print(int i, int n){
-    if(i<1)
-    return;
-    print(i-1, n);
-    cout<<i<<endl;
-}
-int main() {
-    int n=5;
-    print(n,n);
+// #include <iostream>
+// using namespace std;
+// void print(int i, int n){
+//     if(i<1)
+//     return;
+//     print(i-1, n);
+//     cout<<i<<endl;
+// }
+// int main() {
+//     int n=5;
+//     print(n,n);
 
-    return 0;
+//     return 0;
+// }
+
+
+// 2nd Approach
+
+#include<iostream>
+using namespace std;
+void printTillN(int n){
+    if(n>0){
+        printTillN(n-1);
+        cout<<n<<endl;
+    }
+}
+int main(){
+    int n=5;
+    printTillN(n);
 }
 /*
 1
