6bdea669a324863c6c090345bf7533ed710b30bd
github_username <princekumarsharma365@gmail.com>
2023-02-25 10:00:16 +0530

    mid of linked  list with implementation

--- c++/linkedList/MiddleElementLinkedList.cpp
+++ c++/linkedList/MiddleElementLinkedList.cpp
@@ -38,3 +38,69 @@
     
 
 // };
+
+
+#include <iostream>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+};
+void print(node* n){
+    while(n!=NULL){
+        cout<<n->data<<endl;
+        n=n->next;
+    }
+}
+
+
+// find mid of linked list 
+int mid(node * head){
+    node* slow=head;
+    node* fast=head;
+    
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        
+    }
+    cout<<"Mid of linked list : "<< slow->data;
+    
+}
+int main() {
+    node *head=new node();
+    node *second=new node();
+    node* third=new node();
+    node*forth=new node();
+    node* fifth=new node();
+    
+    
+   head->data=5;
+   head->next=second ;
+   
+   second->data=6;
+   second->next=third;
+   
+   third->data=9;
+   third->next=forth;
+   
+   forth->data=2;
+   forth->next=fifth;
+   
+   fifth->data=10;
+   fifth->next=NULL;
+   
+   print(head);
+   mid(head);
+
+    return 0;
+}
+
+/*
+5
+6
+9
+2
+10
+Mid of linked list : 9
+*/
