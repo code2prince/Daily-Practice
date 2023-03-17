390c5b0f2c5573cdb06e90cb885021ee16dc27ea
github_username <princekumarsharma365@gmail.com>
2023-03-17 14:44:22 +0530

    reverse a linked list

--- c++/linkedList/reverseLinkedList.cpp
+++ c++/linkedList/reverseLinkedList.cpp
@@ -54,3 +54,62 @@
         }
         return prev;
         */
+
+
+       // Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+};
+
+void rev(node * head){
+    node* curr=head;
+    node* fwd=NULL;
+    node* prev=NULL;
+    while(curr!=NULL){
+        fwd=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=fwd;
+    }
+    while(prev!=NULL){
+     cout<<prev->data<<" ";
+    prev=prev->next;
+    }
+}
+
+void print(node* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
+int main() {
+    node* head=new node();
+    node* sec=new node();
+    node* third=new node();
+    
+    head->data=1;
+    head->next=sec;
+    
+    sec->data=4;
+    sec->next=third;
+    
+    third->data=5;
+    third->next=NULL;
+    cout<<"Before revese:";
+    print(head);
+    cout<<endl;
+  
+  cout<<"After reverse: ";
+    rev(head);
+    cout<<endl;
+
+    return 0;
+}
+/*
+Before revese:1 4 5 
+After reverse: 5 4 1 
+*/
