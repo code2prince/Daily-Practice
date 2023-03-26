7e824e1592f67494e92523bbdd01958145d0f4a5
github_username <princekumarsharma365@gmail.com>
2023-03-26 15:09:13 +0530

    check palindrome string without using extra space

--- c++/String/palindromString.cpp
+++ c++/String/palindromString.cpp
@@ -3,19 +3,49 @@
 
 // reverse and check >>>>> but it took extra space 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     string  str="noon";
+//     int n=str.length();
+
+//     string rev="";
+//     for(int i=n-1; i>=0; i--){
+//         rev+=str[i];
+//     }
+//     if(rev==str){
+//         cout<<"palindrome"; // palindrome
+//     }else{
+//         cout<<"not";
+//     }
+// }
+
+
+// solve without using extra space 
+
 #include<iostream>
 using namespace std;
-int main(){
-    string  str="noon";
-    int n=str.length();
-
-    string rev="";
-    for(int i=n-1; i>=0; i--){
-        rev+=str[i];
+bool checkPalindrome(string str, int n){
+    int s=0;
+    int e=n-1;
+    while(s<e){
+        if(str[s]!=str[e]){
+            return false;
+        }else{
+            s++;
+            e--;
+        }
     }
-    if(rev==str){
-        cout<<"palindrome"; // palindrome
+    return true;
+}
+
+int main(){
+    string str="madam";
+    int n=str.length();
+    
+    if(checkPalindrome(str, n)){
+        cout<<"its a palindrome";  //its a palindrome
     }else{
-        cout<<"not";
+        cout<<"not a palindrome";
     }
 }
