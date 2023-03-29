2641f4e42ba79ce413a86c6ecb0098c4f494ebf0
github_username <princekumarsharma365@gmail.com>
2023-03-29 18:42:47 +0530

    sort 0  1 2 into linked list

--- c++/linkedList/sort0s1s2sintoLinkedList.cpp
+++ c++/linkedList/sort0s1s2sintoLinkedList.cpp
@@ -54,3 +54,72 @@ class Solution
 };
 
 */
+
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+};
+
+void print(node* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
+void sortLinkedList(node* head){
+    node* temp=head;
+    vector<int>v;
+    while(temp!=NULL){
+        v.push_back(temp->data);
+        temp=temp->next;
+    }
+    sort(v.begin(), v.end());
+
+    int i=0; 
+    temp=head;
+    while(temp!=NULL){
+        temp->data=v[i];
+        i++;
+        temp=temp->next;
+    }
+
+    while(head!=NULL){
+    cout<<head->data<<" ";
+    head=head->next;
+    } 
+}
+int main() {
+    node* head=new node();
+    node* sec=new node();
+    node* third=new node();
+    node* forth=new node();
+    
+    head->data=1;
+    head->next=sec;
+    
+    sec->data=0;
+    sec->next=third;
+    
+    third->data=2;
+    third->next=forth;
+
+    forth->data=1;
+    forth->next=NULL;
+    
+    cout<<"Before sorting: ";
+    print(head);
+    cout<<endl;
+
+     cout<<"after sorting: ";
+    sortLinkedList(head);
+
+    return 0;
+}
+
+/*
+Before sorting: 1 0 2 1 
+after sorting: 0 1 1 2
+*/
