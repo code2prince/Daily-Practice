b99f5b360e33151f7a2dd0ac956a921ea8f1f083
github_username <princekumarsharma365@gmail.com>
2023-04-20 12:32:59 +0530

    object creation dynamically and scope of access modifier

--- c++/oops/AccessModifier/accessModifier.cpp
+++ c++/oops/AccessModifier/accessModifier.cpp
@@ -1,3 +1,7 @@
+// Access Modifier
+
+
+
 // how to create and class
 
 /*
@@ -13,7 +17,13 @@ class Hero{
 // create an object of the class Hero
 
 /*
+>>>> Statically  object creation
 Hero h1;
+*/
+
+/*
+Dynamically object create
+Hero *h= new Hero;
 */
 
 /*
@@ -37,6 +47,7 @@ int main(){
 
 */
 
+/*
 #include<iostream>
 using namespace std;
 
@@ -49,13 +60,46 @@ class hero{
 };
 
 int main(){
-    
+
     // create a object of class hero
     hero h1;
     //set data
     h1.health=70;
 
     cout<<"Health is:" <<h1.health<<endl;   // Health is:70
+}
+*/
 
+#include<iostream>
+using namespace std;
+
+// create a class
+class hero{
+    // properties
+    public:
+        int health;
+    private:
+        char level;
+
+
+// it can be only and only accessable into scope >> By the help of getter and Setter 
+    void print(){
+        level='A';
+        cout<<"Level is: "<<level;
+    }
+};
+
+int main(){
+
+    // create a object of class hero
+    hero h1;
+
+    //set data
+
+    h1.health=70;
+    // h1.level='c';  >> Error
+
+    cout<<"Health is:" <<h1.health<<endl;   // Health is:70
+    // cout<<"level is: "<<h1.level<<endl;    >>> Error due to private member function can't be access out side the class
 }
 
