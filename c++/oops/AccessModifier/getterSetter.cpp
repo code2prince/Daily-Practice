2ec0d103975a8178e708d32bf6fae23dd4feb97f
github_username <princekumarsharma365@gmail.com>
2023-04-22 09:48:19 +0530

    static and dynamic allocation

--- c++/oops/AccessModifier/getterSetter.cpp
+++ c++/oops/AccessModifier/getterSetter.cpp
@@ -16,7 +16,7 @@ class Hero{
 
    
 
-    //getter
+    //getter   >> used for fetch the data
     int getHealth(){
         return health;
     }
@@ -25,7 +25,7 @@ class Hero{
         return level;
     }
 
-    //setter
+    //setter   >> used to set data conditional set into data Eg. password setting condition
     void setHealth(int h){
         health=h;
     }
@@ -35,15 +35,44 @@ class Hero{
     }
 };
 int main(){
+    // Static object creation
     Hero h1;
 
+     //////static
 
     cout<<"health is: "<<h1.getHealth()<<endl;  //health is: 6422376 >>> garbage value
     // setter >> set health
     h1.setHealth(80);
-    h1.level='A';
+    //h1.level='A';
+    h1.setLevel('A');
 
     cout<<"health is: "<<h1.getHealth()<<endl;  // health is: 80
     cout<<"level is: "<<h1.getLevel()<<endl; // level is: A
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+    // Dynamically object creation  >> data store in Heap Memory
+    Hero *h2= new Hero;
+
+    h2->setHealth(20);
+    
+    //h2->level='B';
+    // or
+    h2->setLevel('B');
+
+
+    cout<<"health is..: "<<(*h2).getHealth()<<endl;  // health is..: 20
+    // or 
+    cout<<"health is .......: "<< h2->getHealth()<<endl;  //health is .......: 20
+
+    cout<<"level is ..: "<<(*h2).level<<endl;  // level is ..: B
+    //or 
+    cout<<"level is...:"<<h2->level<<endl;    // level is ...: B
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+   
 
 }
