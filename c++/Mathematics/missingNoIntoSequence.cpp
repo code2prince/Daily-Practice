08917e3d1037aa4fe6cdc013834de4a88c3416f7
github_username <princekumarsharma365@gmail.com>
2023-04-30 10:28:09 +0530

    Missing No into a sequence

---
+++ c++/Mathematics/missingNoIntoSequence.cpp
@@ -0,0 +1,24 @@
+// Missing No into a sequence
+
+// arr[]={5,7,9,13,15,17};  >> 11 is misssing into AP series 
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[]={5,7,9,13,15,17};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int N=n+1;
+    int d=arr[1]-arr[0];
+    
+    int totalSum=0;
+    totalSum=N*(2*arr[0] +(N-1)*d)/2;
+    cout<<"total sum: "<<totalSum<<endl;  //total sum: 77
+
+   
+    for(int i=0; i<n; i++){
+       totalSum-=arr[i];
+    }
+    cout<<"missing no: "<<totalSum;  // missing no: 11
+
+}
