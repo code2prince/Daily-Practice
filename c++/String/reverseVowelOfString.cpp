30a99381552dc9f981182e9aaabc0d46b063978a
github_username <princekumarsharma365@gmail.com>
2023-08-24 23:50:47 +0530

    Reverse Vowels of a String

--- c++/String/reverseVowelOfString.cpp
+++ c++/String/reverseVowelOfString.cpp
@@ -44,3 +44,37 @@ bool isVowel(char c){
     }
 };
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+bool isVowel(char c){
+    return c=='a' || c=='e' || c=='i' ||c=='o' ||c=='u' || c=='A' || c=='E' || c=='I'   || c=='O' || c=='U'; 
+}
+string reverseVowel(string s){
+    int i=0;
+    int j=s.length()-1;
+    while(i<j){
+        if(isVowel(s[i]) && isVowel(s[j])){
+            swap(s[i],s[j]);
+            i++;
+            j--;
+        }
+        else if(!isVowel(s[i])){
+            i++;
+        }
+        else if(!isVowel(s[j])){
+            j--;
+        }
+    }
+     return s;
+
+}
+int main(){
+    string s="hello";
+    string reverseVowel(string s);
+    for(int i=0; i<s.length(); i++){
+            cout<<s[i];
+        }
+}
+   
+
