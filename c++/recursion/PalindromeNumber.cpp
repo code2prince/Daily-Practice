83c066dd1ee064d94de6e2beef41c28ee2b7be21
github_username <princekumarsharma365@gmail.com>
2023-10-06 22:05:24 +0530

    -

--- c++/recursion/PalindromeNumber.cpp
+++ c++/recursion/PalindromeNumber.cpp
@@ -23,3 +23,29 @@ int main(){
     }
 
 }
+
+
+/*    --------Normally---------
+#include<iostream>
+using namespace std;
+int main(){
+    int n,temp;
+    int rev=0;
+    cout<<"Enter the no:";
+    cin>>n;
+    temp=n;
+    while(n>0){
+        rev=rev*10 + (n%10);
+        n=n/10;
+    }
+    cout<<"reverse:"<<rev<<endl;
+    if(temp==rev)
+        cout<<"palindrome";
+    
+    else
+        cout<<"not palindrome";
+    
+    return 0;
+}
+
+*/
